3077dc8bd3c4ee7f2d4c1ebeeb311adb0a5dfd12
Blues0300 <jpcruzjp@gmail.com>
2023-07-18 22:56:32 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("form");
+  const buttonCopyText = document.getElementById("btn-copy");
+  
+  let formattedOutput;
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const inputData = document.getElementById("text-input").value;
+    formattedOutput = formatarTexto(inputData);
+    document.querySelector(".output-text").innerText = formattedOutput;
+  });
+  
+  buttonCopyText.addEventListener("click", function () {   
+    if (!formattedOutput) return; 
+    console.log(formattedOutput)    
+    navigator.clipboard.writeText(formattedOutput);
+    alert("Título Copiado");
+  });
+  
+  
+});
+
+function formatarTexto(inputData) {
+  const parts = inputData.split("\t");
+  
+  
+  if (parts.length !== 12) { 
+    return  "Formato Inválido";
+  }
+
+  const data = parts[1].split("/");
+  const hora = parts[11].split(":");
+  const duracao = parts[6].split(":");
+  const output =
+    data[0] +
+    " " +
+    data[1] +
+    " " +
+    data[2] +
+    " - " +
+    hora[0] +
+    "h" +
+    hora[1] +
+    " - " +
+    parts[5] +
+    " - " +
+    parts[3] +
+    " - " +
+    duracao[0] +
+    "´" +
+    duracao[1] +
+    "´" +
+    duracao[2];
+  return output;
+}
+
